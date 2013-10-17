0605667b65603c763e3824a11ed95aedf234b655
VarsitySoftware <github@varsitysoftware.com>
2013-10-17 09:02:03 -0400

    Update NativeVideoPlayer.m

--- src/ios/NativeVideoPlayer.m
+++ src/ios/NativeVideoPlayer.m
@@ -5,3 +5,24 @@
 //  Created by Varsity Software, Inc on 10-17-13.
 //  MIT Licensed
 //  Copyright 2013 Decaf Varsity Software, Inc. All rights reserved.
+
+#import "Echo.h"
+#import <Cordova/CDV.h>
+
+@implementation Echo
+
+- (void)echo:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end

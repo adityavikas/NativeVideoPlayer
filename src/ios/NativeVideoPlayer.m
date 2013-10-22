9adcd8565cbb20fe98de595c4c28356c111fc247
VarsitySoftware <github@varsitysoftware.com>
2013-10-21 23:47:34 -0400

    Update NativeVideoPlayer.m

--- src/ios/NativeVideoPlayer.m
+++ src/ios/NativeVideoPlayer.m
@@ -7,22 +7,66 @@
 //  Copyright 2013 Decaf Varsity Software, Inc. All rights reserved.
 
 #import "NativeVideoPlayer.h"
+#import "MediaPlayer/MPMoviePlayerViewController.h"
+#import "MediaPlayer/MPMoviePlayerController.h"
+#import <Cordova/CDVViewController.h>
 #import <Cordova/CDV.h>
 
-@implementation Echo
+@implementation NativeVideoPlayer
 
-- (void)echo:(CDVInvokedUrlCommand*)command
+- (void)play:(CDVInvokedUrlCommand*)command
 {
+    //NSString* callbackId = command.callbackId;
+    NSDictionary* options = [command.arguments objectAtIndex:0];
+    
+    if ([options isKindOfClass:[NSNull class]]) {
+        options = [NSDictionary dictionary];
+    }
+    
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    
+    NSString* filePath = [command.arguments objectAtIndex:0];
+    
+    if (filePath != nil && [filePath length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: filePath];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
+    
+    MPMoviePlayerViewController *playerVC = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:filePath]] ;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(MovieDidFinish:) name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
 
+    playerVC.moviePlayer.controlStyle = MPMovieControlStyleNone;
+    playerVC.moviePlayer.shouldAutoplay = YES;
+    
+    playerVC.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    
+    [self.viewController presentViewController:playerVC animated:NO completion:nil];
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+- (void)MovieDidFinish:(NSNotification *)notification {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+    
+    int reason = [[[notification userInfo] valueForKey:MPMoviePlayerPlaybackDidFinishReasonUserInfoKey] intValue];
+    if (reason == MPMovieFinishReasonPlaybackEnded) {
+        //movie finished playin
+    }else if (reason == MPMovieFinishReasonUserExited) {
+        //user hit the done button
+    }else if (reason == MPMovieFinishReasonPlaybackError) {
+        //error
+    }
+    
+    self.viewController.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    [self.viewController dismissViewControllerAnimated:NO completion:nil];
+    
+    [self.viewController removeFromParentViewController];
+    
+    [self writeJavascript:[NSString stringWithFormat:@"onVideoEnd(\"%d\");", reason]];
+    
+}
+
 
 @end

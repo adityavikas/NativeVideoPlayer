d970ce3eeedf93c5c013105d466985e99af42a6b
VarsitySoftware <github@varsitysoftware.com>
2013-10-21 23:43:35 -0400

    Update NativeVideoPlayer.h

--- src/ios/NativeVideoPlayer.h
+++ src/ios/NativeVideoPlayer.h
@@ -4,7 +4,7 @@
 //
 //  Created by Varsity Software, Inc on 10-17-13.
 //  MIT Licensed
-//  Copyright 2013 Varsity Software, Inc. All rights reserved.
+//  Copyright 2013 Decaf Varsity Software, Inc. All rights reserved.
 
 #import <Cordova/CDV.h>
 
